2c41ed7396781eda97bbf7abcf0bdc3e89d96333
Ilian Andrianov <ilian.and.04@gmail.com>
2023-03-23 21:46:42 +0300

    Thu Mar 23 09:46:42 PM MSK 2023

--- modules/commit_modules.py
+++ modules/commit_modules.py
@@ -5,12 +5,13 @@
       shell: 0.2.0
     needs_pip: {}
     once: false
-    origin: https://t.me/c/1796785408/3581
+    origin: https://t.me/c/1796785408/3593
     priority: 34
-    version: 0.1.2
+    version: 0.1.3
     wants: {}
 """
 from tgpy.api import config
+
 
 def save_registry():
     reg = '"""\n    sources:\n' + \
@@ -19,11 +20,12 @@ def save_registry():
     with open('tgpy-modules-pack/registry.txt', 'w') as f:
         f.write(reg)
 
+
 async def commit_modules():
     return await shell("""
 cd tgpy-modules-pack
 git pull
 git add .
-git commit -m "date"
+git commit -m "`date`"
 git push
 """)
